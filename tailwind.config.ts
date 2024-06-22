f142f928e68e7d64f950a0e00b3148c007b8f94a
Pedro Grigorio <pedroogrigorio@gmail.com>
2024-06-22 17:54:46 -0300

    config colors in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,9 +16,37 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      colors: {
+        blue: {
+          '50': '#eff4fb',
+          '100': '#cddcf3',
+          '200': '#b5cbed',
+          '300': '#93b4e5',
+          '400': '#7ea5e0',
+          '500': '#5e8fd8',
+          '600': '#436699',
+          '700': '#161B29',
+          '800': '#040813',
+          '900': '#02050D',
+        },
+
+        gray: {
+          '50': '#e8e8e8',
+          '100': '#C9C9C9',
+          '200': '#B7B7B7',
+          '300': '#949494',
+          '400': '#636363',
+          '500': '#454545',
+          '600': '#2D1E1E',
+          '700': '#161616',
+          '800': '#121212',
+          '900': '#090909',
+        },
+      },
     },
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [],
 }
 
 export default config
